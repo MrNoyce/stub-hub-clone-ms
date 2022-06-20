b31bf95bec27cb619175629b89195e4bc53e1b52
MrPanda <noyce.consulting@gmail.com>
2022-06-20 22:43:33 +0100

    adding basic tests, spliting index.ts to be able to pass app into out tests env, updating docker file to build only prod deps

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,42 +1,7 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-const app = express();
-app.set('trust proxy', true);
-app.use(json());
-app.use(
-	cookieSession({
-		signed: false,
-		secure: true,
-	})
-);
-
-// router handler configurations
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-// handle default root error for all request types
-app.all('*', async (req, res, next) => {
-	throw new NotFoundError();
-});
-
-// error handling middleware
-app.use(errorHandler);
+import { app } from './app';
 
 const start = async () => {
-
 	if(!process.env.JWT_KEY) {
 		throw new Error('JWT_KEY must be defined');
 	}
